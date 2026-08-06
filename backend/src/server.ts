cc2a77368b1b0bdbd60cb729465e3ec63fd4f98d
kaiserraf <rafaelmachado2507oliveira@hotmail.com>
2026-08-06 19:23:49 -0300

    fix: não entrava, agora só tem alguns bugs no backend e tmb ta bem feio o site

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './router';
+import path from 'node:path';
 
 const PORT = process.env.PORT || '3000'
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../frontend')));
 app.use('/', router);
 
 app.use('/', (_req, res) => {
